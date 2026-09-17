6a61e38561acc22e8a2e4728dcdad831cc0fea9c
Martin Zhang <martinh.zhang@gmail.com>
2026-09-17 15:53:03 +0200

    [survival simulator] make sim_time and n_agents optional

--- survival-simulator/src/utils/DTOs.py
+++ survival-simulator/src/utils/DTOs.py
@@ -37,6 +37,6 @@ class StepResponse(BaseModel):
     """
     game_status: str
     score: float
-    sim_time: [Optional]float = None
-    n_agents: [Optional]int = None
+    sim_time: Optional[float] = None
+    n_agents: Optional[int] = None
     agent_status: List[ObservationResponse] = []
